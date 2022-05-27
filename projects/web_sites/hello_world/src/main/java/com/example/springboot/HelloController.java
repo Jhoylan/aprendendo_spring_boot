3546506a9a373802285212a5dfb7c52e73aa5a01
Jhoylan <jhoylan.santos@jambu.com.br>
2022-05-27 11:18:07 -0300

    hello world

---
+++ projects/web_sites/hello_world/src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "home";
+    }
+
+}
